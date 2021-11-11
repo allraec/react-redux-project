0e52cf194540f172a054096ec6aba7162637ce8b
Allison Castaneda <allraec.dev@gmail.com>
2021-11-10 20:40:06 -0600

    Added NoteForm component

---
+++ src/components/NoteForm.js
@@ -0,0 +1,15 @@
+const NoteForm = () => {
+    return(
+        <form>
+            <label>
+                <input type="text"/>
+            </label>
+            <br/>
+            <label>
+                <textarea/>
+            </label>
+        </form>
+    );
+}
+
+export default NoteForm;
